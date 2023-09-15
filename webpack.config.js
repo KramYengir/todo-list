02ff44b2f56d9d0981024e390ced9ccfa194d7bb
KramYengir <kramyengir@gmail.com>
2023-09-15 15:26:07 +0100

    setup webpack & organize directory

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        assetModuleFilename: '[name][ext]'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        open: true,
+        hot: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.s?css$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource'
+          }
+        ],
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Toodle-Do',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ],
+};
