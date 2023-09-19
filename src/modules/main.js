8b5c4582c64fcccd49080c73c38f061b8222db29
KramYengir <kramyengir@gmail.com>
2023-09-19 13:55:04 +0100

    add some comments

--- src/modules/main.js
+++ src/modules/main.js
@@ -2,11 +2,13 @@ const Main = (()=>{
 
     const mainContainer = document.querySelector('main');
 
+    //load content into the main area
     const loadContent = (content)=>{
         clearContent();
         mainContainer.appendChild(content);
     }
 
+    //clear all content before loading new content
     const clearContent = ()=>{
         while (mainContainer.firstChild) {
             mainContainer.removeChild(mainContainer.firstChild);
