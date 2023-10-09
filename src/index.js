23a9b626b95794f1028b9a40a893a35703bd1dfe
KramYengir <kramyengir@gmail.com>
2023-10-09 17:59:36 +0100

    fixed unused vars errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles/main.scss";
 import "./styles/todo.scss";
-import Modal from "./modules/modal";
-import Nav from "./modules/navEvents";
+import "./modules/modal";
+import "./modules/navEvents";
 import Main from "./modules/main";
 
 Main.loadTodayTodos();
